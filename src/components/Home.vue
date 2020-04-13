8e8e5c5dc30e5146c0829afb304f2f0dfb63b929
lianyu <1774785796@qq.com>
2020-04-13 15:58:20 +0800

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,14 @@
 <template>
-      <div>
-            <el-button type="info" @click="exit">退出</el-button>
-      </div>
+      <el-container>
+            <el-header>
+                  Header
+                  <el-button type="info" @click="exit">退出</el-button>
+            </el-header>
+            <el-container>
+                  <el-aside width="200px">Aside</el-aside>
+                  <el-main>Main</el-main>
+            </el-container>
+      </el-container>
 </template>
 
 <script>
